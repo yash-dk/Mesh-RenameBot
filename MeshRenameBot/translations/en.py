09bfb43df8440d06d3c4b018da001d6cfca37f17
yash-dk <yashkhadse@gmail.com>
2021-06-20 13:22:21 +0530

    Added Translations to Rename

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -1,4 +1,21 @@
 class EnTrans:
+    
     WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
+    
     START_MSG = "Hello there, I am Mesh Rename Bot."
+    
     CANCEL_MESSAGE = "The rename has been cancled. Will be updated soon."
+    
+    RENAME_NO_FILTER_MATCH = "NO FILTER MATCHED ABORTING RENAME \nUsing the filters to Rename. As no name was specified. Manage your Filters using /filters."
+
+    RENAME_FILTER_MATCH_USED = "Using the filters to Rename. As no name was specified. Manage your Filters using /filters"
+
+    RENAME_NOFLTR_NONAME = "Enter the rename file name in format :- ```/rename my new filename.extension``` or ```Use /filters to set some rename filters.```"
+
+    RENAME_CANCEL = "Cancel this rename."
+
+    RENAMING_FILE = "Renaming the file currently hold on."
+
+    RENAME_ERRORED_REPORT = "The download was errored. Report this error."
+
+    RENAME_CANCEL_BY_USER = "Canceled by the user."
