956383c59b329844546d3f5e767ae6b31dd99344
yash-dk <yashkhadse@gmail.com>
2021-07-05 12:36:22 +0530

    give specific dir

--- MeshRenameBot/maneuvers/Rename.py
+++ MeshRenameBot/maneuvers/Rename.py
@@ -75,7 +75,10 @@ class RenameManeuver(DefaultManeuver):
 
         try:
             progress = await self._media_message.reply(Trans.DL_RENAMING_FILE, quote=True, reply_markup=markup)
+            dl_path = os.path.join("downloads/{}/".format(str(time.time()).replace(".","")))
+            os.makedirs(dl_path, exist_ok=True)
             dl_path = await self._media_message.download(
+                file_name=dl_path,
                 progress=progress_for_pyrogram, 
                 progress_args=(
                     "Downloading the file",
