d3e2467e7aad7fac9e18ff59a0bf066c17680102
yash-dk <yashkhadse@gmail.com>
2021-07-07 19:17:35 +0530

    Fix typo

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -4,7 +4,7 @@ class EnTrans:
     
     START_MSG = "Hello there, I am Mesh Rename Bot. Open Source at https://github.com/yash-dk/Mesh-RenameBot/tree/master. You can deploy your own."
      
-    CANCEL_MESSAGE = "The rename has been cancled. Will be updated soon."
+    CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
     
     RENAME_NO_FILTER_MATCH = "NO FILTER MATCHED ABORTING RENAME \nUsing the filters to Rename. As no name was specified. Manage your Filters using /filters."
 
