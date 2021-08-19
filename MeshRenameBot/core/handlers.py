55acce1ea06b28e77607a460a11db70444b34379
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-08-19 16:01:19 +0530

    Remove @ stuff its useless & replace it with url

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -125,12 +125,7 @@ async def intercept_handler(client: Client, msg: Message) -> None:
                 return
         except UserNotParticipant:
             forcejoin = get_var("FORCEJOIN")
-            if "@" in forcejoin:
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton('join Updates channel', url=f'https://t.me/{forcejoin}')]])
-            else:
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton('join Updates channel', url=forcejoin)]])
-            
-            await msg.reply_text("Join the given chat in order to use this bot.", reply_markup=markup)
+            await msg.reply_text("Join the given chat in order to use this bot.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Updates Channel", url=f"{forcejoin}")]]),parse_mode="markdown"))
             return
         except ChatAdminRequired:
             renamelog.error("The bot is not the admin in the chat make it admin first.")
