2d590f2629b98f30e3df63cc7adf1f97585ae7d4
nitanuj <103054544+nitanuj@users.noreply.github.com>
2022-06-08 18:44:45 +0530

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -1,9 +1,16 @@
-
+import os
+from telethon import Button
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 class EnTrans:
     
     WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
     
-    START_MSG = "Hello There  ,I Am 𝚁𝚎𝚒 𝚂𝚞𝚣𝚞𝚔𝚊𝚠𝚊 A Renamer Bot."
+    START_MSG = "Hello There  ,I Am <a href='https://telegra.ph/file/b10b0620a1129844a2bc8.jpg'> 𝚁𝚎𝚒 𝚂𝚞𝚣𝚞𝚔𝚊𝚠𝚊 </a>A Renamer Bot."
+    START_MSG =  InlineKeyboardMarkup([
+		[InlineKeyboardButton('⚡ Developer ⚡', url='https://t.me/TharukRenuja'),
+		 InlineKeyboardButton(' Owner ', url='https://t.me/Violetadvance')],
+		[InlineKeyboardButton('💠 Help', callback_data='help'),
+		 InlineKeyboardButton('Close ✖️', callback_data='close')]
      
     CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
     
