361677b45bbad7ff7cbeb78b2084cbae08c74c6d
nitanuj <103054544+nitanuj@users.noreply.github.com>
2022-06-19 22:02:50 +0530

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -1,9 +1,9 @@
 import os
 class EnTrans:
-    mentionUser = f"<a href='tg://user?id=<user_id>'</a>"
+    <a href='tg://user?id=<user_id>'</a>
     WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
     
-    START_MSG = "𝐇𝐞𝐲 {mentionUser} ,𝐈 𝐀𝐦 𝐀 𝐑𝐞𝐧𝐚𝐦𝐞𝐫 𝐁𝐨𝐭.𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫 𝐂𝐚𝐥𝐥 𝐌𝐞 𝐑𝐞𝐢<a href='https://telegra.ph/file/752202a74137ca11ddca3.jpg'>😺</a>"
+    START_MSG = "𝐇𝐞𝐲  <a href='tg://user?id=<user_id>'</a> ,𝐈 𝐀𝐦 𝐀 𝐑𝐞𝐧𝐚𝐦𝐞𝐫 𝐁𝐨𝐭.𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫 𝐂𝐚𝐥𝐥 𝐌𝐞 𝐑𝐞𝐢<a href='https://telegra.ph/file/752202a74137ca11ddca3.jpg'>😺</a>"
 	
     CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
     
