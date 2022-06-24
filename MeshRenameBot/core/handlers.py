00bd7f69281e1cbcaf587825d2ee68032cc1d362
nitanuj <103054544+nitanuj@users.noreply.github.com>
2022-06-24 12:58:04 +0530

    Update handlers.py

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -124,7 +124,7 @@ async def intercept_handler(client: Client, msg: Message) -> None:
                 return
         except UserNotParticipant:
             forcejoin = get_var("FORCEJOIN")
-            await msg.reply_text("Join the given chat in order to use this bot.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Hentai Goated", url=f"{forcejoin}")]]))
+            await msg.reply_text("Join the given chat in order to use this bot.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Update Channel", url=f"{forcejoin}")]]))
             return
         except ChatAdminRequired:
             renamelog.error("The bot is not the admin in the chat make it admin first.")
